0f697b3067db10c8755c2aa95e2f59ad1d9a97d7
Rajesh kumar <53404249+rajesh0305@users.noreply.github.com>
2024-11-04 11:08:08 +0530

    Time: 1 ms (67.83%) | Memory: 44.6 MB (61.00%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,19 +1,34 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-         if(nums.length == 0)
+    public static int optimal(int nums[]){
+        int n = nums.length;
+        if(n==0){
             return 0;
-        
-        int addIndex = 1; //index that unique characters will be inserted at
-
-        for(int i = 0; i < nums.length - 1; i++) 
-        {
-            
-            if(nums[i] < nums[i + 1])
-            { //if true, num[i + 1] is a new unique number
-              nums[addIndex] = nums[i + 1];
-              addIndex++;
+        }
+        int i = 0;
+        for(int j = 1;j<n;j++){
+            if(nums[i]!=nums[j]){
+                nums[i+1] = nums[j];
+                i++;
             }
         }
-        return addIndex;
+        return i+1;
+    }
+    public int removeDuplicates(int[] nums) {
+        //  if(nums.length == 0)
+        //     return 0;
+        
+        // int addIndex = 1; //index that unique characters will be inserted at
+
+        // for(int i = 0; i < nums.length - 1; i++) 
+        // {
+            
+        //     if(nums[i] < nums[i + 1])
+        //     { //if true, num[i + 1] is a new unique number
+        //       nums[addIndex] = nums[i + 1];
+        //       addIndex++;
+        //     }
+        // }
+        // return addIndex;
+        return optimal(nums);
     }
 }
